846f80e4e8dcb6ed9dd213a949be9aec5b68ea3b
Sirbuni <e.sabuni@gmail.com>
2020-09-08 01:10:18 +0300

    Splash screen working

---
+++ app/src/main/java/com/sabunipractice/gadsleaderboard2020/SplashActivity.java
@@ -0,0 +1,40 @@
+package com.sabunipractice.gadsleaderboard2020;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.view.animation.AccelerateInterpolator;
+import android.view.animation.AlphaAnimation;
+import android.widget.ImageView;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private static final int SPLASH_SCREEN_TIMEOUT = 2000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_splash);
+
+        AlphaAnimation fadeOut = new AlphaAnimation(1, 0);
+        fadeOut.setInterpolator(new AccelerateInterpolator());
+        fadeOut.setStartOffset(500);
+        fadeOut.setDuration(2000);
+
+        ImageView appLogo = findViewById(R.id.imageView);
+        appLogo.setAnimation(fadeOut);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, SPLASH_SCREEN_TIMEOUT);
+    }
+}
